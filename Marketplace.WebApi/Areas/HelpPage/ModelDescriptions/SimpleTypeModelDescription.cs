c7d93c400d230ba1706590a4dd86ba31c184d4c1
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-09-11 14:34:48 -0400

    Add project files.

---
+++ Marketplace.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Marketplace.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Marketplace.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Marketplace.WebApi.WebApiApplication" Language="C#" %>
