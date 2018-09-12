bd721b03372383c24e4fd3d310efa55b8bd9b9ad
Bailey Riker <baileyjamesriker@gmail.com>
2018-09-12 11:35:46 -0400

    changes-Services

--- Marketplace.Services/CustomerService.cs
+++ Marketplace.Services/CustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Marketplace.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,40 @@ namespace Marketplace.Services
 {
      public class CustomerService
     {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public CustomerDetails GetCustomerById(int CustomerId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.CustomerID == CustomerId && e.OwnerId == _userId);
+                return
+                    new CustomerDetails
+                    {
+                        CustomerId = entity.NoteId,
+                        CustomerFirstName = entity.Title,
+                        CUstomerLastName = entity.Content,
+                        CustomerEmail = entity.CreatedUtc,
+                        CustomerStreetAddress = entity.ModifiedUtc,
+                        State = entity.State,
+                        City = entity.City,
+                        Zip = entity.Zip,
+                        CustomerPhone = entity.CustomerPhone,
+                        ShippingInformation = entity.
+
+
+
+
+                    };
+            }
+        }
     }
 }
