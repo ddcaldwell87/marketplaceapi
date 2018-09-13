8ba526f4176453b7caad83f6ef2bc4a8a6fbb671
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-09-13 16:42:28 -0400

    Started adding admin role

---
+++ Marketplace.Services/AdminService.cs
@@ -0,0 +1,65 @@
+﻿using Marketplace.Contracts;
+using Marketplace.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly Guid _userId;
+
+        public AdminService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<IdentityRole> GetRolesList()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var rolesList = ctx.Roles.ToList();
+                return rolesList.ToArray();
+            }
+        }
+
+        public IEnumerable<ApplicationUser> GetUserList()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var userList = ctx.Users.ToList();
+                return userList.ToArray();
+            }
+        }
+
+        public bool IsAdminUser()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                try
+                {
+                    var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ctx));
+                    var s = userManager.GetRoles(_userId.ToString());
+                    if (s.Count != 0 && s[0].ToString() == "Admin")
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
